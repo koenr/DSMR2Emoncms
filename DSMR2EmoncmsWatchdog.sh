06eb79e1cc83e8d4d443417cfe09e0a2ed3b023b
Koen Roggemans <koen@roggemans.net>
2026-02-26 19:15:35 +0100

    Update DSMR2EmoncmsWatchdog.sh
    python3
    Small bugfixes

--- DSMR2EmoncmsWatchdog.sh
+++ DSMR2EmoncmsWatchdog.sh
@@ -2,56 +2,56 @@
 #
 # Watchdog file for checking correct behaviour of main process
 #
-# Auter : Edwin Bontenbal
-# Email : Edwin.Bontenbal@Gmail.COM
-
+# Auteur : Edwin Bontenbal
+# Email  : Edwin.Bontenbal@Gmail.COM
 FILE="/tmp/DSMR2Emoncms_Watchdog"
 LOGFILE="/var/log/DSMR2Emoncms_Watchdog.log"
-PROCESS="python /usr/local/bin/DSMR2Emoncms.py"
+PROCESS="/usr/bin/python3 /usr/local/bin/DSMR2Emoncms.py"
 TimeNow=$(date +%s)
-ProcessID=$(ps -ef|grep -v grep|grep "$PROCESS"| cut -c 6-14)
+ProcessID=$(pgrep -f DSMR2Emoncms.py)
 
 WriteLog() {
-        echo "$(date) $1" >> $LOGFILE
+    echo "$(date) $1" >> $LOGFILE
 }
 
 if [ -f "$FILE" ];
 then
-   # File exists
-   TimeInWatchdogFile=`cat "$FILE"`
-   TimeDifference=$((TimeNow-TimeInWatchdogFile))
-   if [ "$TimeDifference" -gt "20" ] && [ "$TimeInWatchdogFile" != "" ];
-   then
-      # Time difference is to big  
-      WriteLog "Watchdog file found."
-      WriteLog "Time difference is to big ($TimeDifference), now it is $TimeNow and the file contains ($TimeInWatchdogFile)." 
-	if [ -z "$ProcessID" ];
-      then
-	 # No process found so start it
-         WriteLog "No ProcessID ($ProcessID) belonging to Process($PROCESS) found, start it!"
-         $PROCESS 2>&1 >/dev/null &
-      else
-	 # Process seems to be running, but something is wrong so kill it, and start is afterwards
-         WriteLog "ProcessID ($ProcessID) belonging to Process($PROCESS) found, but something is wrong, kill it hard, and start it!" 
-         kill -9 "$ProcessID"
-         $PROCESS 2>&1 >/dev/null &
-      fi
+    # File exists
+    TimeInWatchdogFile=$(cat "$FILE")
+    TimeDifference=$((TimeNow - TimeInWatchdogFile))
 
-   fi
-
+    # Check leegte eerst, daarna pas het tijdsverschil berekenen (fix: volgorde omgedraaid)
+    if [ "$TimeInWatchdogFile" != "" ] && [ "$TimeDifference" -gt "60" ];
+    then
+        # Time difference is too big
+        WriteLog "Watchdog file found."
+        WriteLog "Time difference is too big ($TimeDifference), now it is $TimeNow and the file contains ($TimeInWatchdogFile)."
+        if [ -z "$ProcessID" ];
+        then
+            # No process found, start it
+            WriteLog "No ProcessID ($ProcessID) belonging to Process ($PROCESS) found, start it!"
+            $PROCESS >/dev/null 2>&1 &
+        else
+            # Process seems to be running but something is wrong, kill it and restart
+            WriteLog "ProcessID ($ProcessID) belonging to Process ($PROCESS) found, but something is wrong, kill it hard and start it!"
+            kill -9 "$ProcessID"
+            rm -f "$FILE"
+            $PROCESS >/dev/null 2>&1 &
+        fi
+    fi
 else
-   # File does not exist, start it if no process is running otherwise kill and start it
-      WriteLog "Watchdog file does nog exist" 
-      if [ -z "$ProcessID" ];
-      then
-         # No process found so start it
-         WriteLog "No ProcessID ($ProcessID) belonging to Process($PROCESS) found, start it!"
-         $PROCESS 2>&1 >/dev/null &
-      else
-         # Process seems to be running, but something is wrong so kill it, and start is afterwards
-         WriteLog "ProcessID ($ProcessID) belonging to Process($PROCESS) found, but something is wrong, kill it hard, and start it!"
-         kill -9 "$ProcessID"
-         $PROCESS 2>&1 >/dev/null &
-      fi
+    # File does not exist
+    WriteLog "Watchdog file does not exist."
+    if [ -z "$ProcessID" ];
+    then
+        # No process found, start it
+        WriteLog "No ProcessID ($ProcessID) belonging to Process ($PROCESS) found, start it!"
+        $PROCESS >/dev/null 2>&1 &
+    else
+        # Process seems to be running but something is wrong, kill it and restart
+        WriteLog "ProcessID ($ProcessID) belonging to Process ($PROCESS) found, but something is wrong, kill it hard and start it!"
+        kill -9 "$ProcessID"
+        rm -f "$FILE"
+        $PROCESS >/dev/null 2>&1 &
+    fi
 fi
-
